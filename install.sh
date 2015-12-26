f7bd473409e318d73fc81308c99a588840c53d8d
Zet <2etx01@gmail.com>
2015-12-26 14:05:31 +0800

    add gitconf

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 ln -fs `pwd`/vimrc    ~/.vimrc
 ln -fs `pwd`/tmux.conf    ~/.tmux.conf
 ln -fs `pwd`/gdbinit    ~/.gdbinit
+ln -fs `pwd`/gitconfig    ~/.gitconfig
 ln -fs `pwd`/zshrc    ~/.zshrc
 ln -fs `pwd`/zet.zsh-theme    ~/.oh-my-zsh/themes/zet.zsh-theme

f6b966ac7713b28751d255a8c2b10dda4ebe0235
Zet <zetpc235@gmail.com>
2016-03-06 06:51:08 +0000

    rm zsh framework

--- install.sh
+++ install.sh
@@ -5,14 +5,3 @@ ln -fs `pwd`/gdbinit    "${HOME}/.gdbinit"
 ln -fs `pwd`/gitconfig    "${HOME}/.gitconfig"
 ln -fs `pwd`/zshrc    "${HOME}/.zshrc"
 ln -fs `pwd`/vim "${HOME}/.vim"
-
-if [ ! -d "${ZDOTDIR:-$HOME}/.oh-my-zsh" ]; then
-    git clone https://github.com/robbyrussell/oh-my-zsh.git "${HOME}/.oh-my-zsh"
-fi
-ln -fs `pwd`/zet.zsh-theme    "${HOME}/.oh-my-zsh/themes/zet.zsh-theme"
-
-if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
-    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-fi
-ln -fs `pwd`/zpreztorc   "${HOME}/.zpreztorc"
-ln -fs `pwd`/prompt_zet_setup    "${HOME}/.zprezto/modules/prompt/functions/prompt_zet_setup"
